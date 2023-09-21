56e265e4e9e8eac730d01635b2f19e0a3159f567
Jax-Hax <jaxhacks543@gmail.com>
2023-09-21 17:26:46 -0400

    seperating into a function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use glam::{Vec3, Quat};
-use vertix::{prelude::*, engine::WorldSpace, camera::{Camera, default_3d_cam}};
+use glam::{Quat, Vec3};
+use hecs::QueryMut;
+use vertix::{
+    camera::{default_3d_cam, Camera},
+    engine::WorldSpace,
+    prelude::*,
+};
+use wgpu::Queue;
 fn main() {
     pollster::block_on(run());
 }
+struct Boid();
 #[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
-    let camera = Camera::new(Vec3::new(0.0, 5.0, 10.0), f32::to_radians(-90.0), f32::to_radians(-20.0));
+    let camera = Camera::new(
+        Vec3::new(0.0, 5.0, 10.0),
+        f32::to_radians(-90.0),
+        f32::to_radians(-20.0),
+    );
     // State::new uses async code, so we're going to wait for it to finish
     let (mut state, event_loop) = State::new(true, env!("OUT_DIR"), camera, 5.0, 2.0).await;
     //add models
@@ -19,33 +30,35 @@ pub async fn run() {
                     let z = SPACE_BETWEEN * (z as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
                     let y = SPACE_BETWEEN * (y as f32 - NUM_INSTANCES_PER_ROW as f32 / 2.0);
                     let position = Vec3 { x, y, z };
-    
-                    let rotation = Quat::from_axis_angle(position.normalize(), f32::to_radians(45.0));
-    
+
+                    let rotation =
+                        Quat::from_axis_angle(position.normalize(), f32::to_radians(45.0));
+
                     Instance { position, rotation }
                 })
             })
         })
         .collect::<Vec<_>>();
-    let (container, is_dynamic) = state
+    let (container, _is_dynamic) = state
         .create_model_instances("cube.obj", instances, true)
         .await;
-    match is_dynamic {
-        Some(_) => state.world.spawn((container, IsDynamic,WorldSpace)),
-        None => state.world.spawn((container,WorldSpace)),
-    };
+    state.world.spawn((container, IsDynamic, WorldSpace, Boid));
     //render loop
     run_event_loop(state, event_loop, update, keyboard_input, default_3d_cam);
 }
 fn update(state: &mut State) {
-    for (_entity, (game_object, _)) in state
+    let boids = state
         .world
-        .query_mut::<(&mut InstanceContainer, &IsDynamic)>()
-    {
+        .query_mut::<(&mut InstanceContainer, &IsDynamic)>();
+    check_boid_collision(&state.queue,boids );
+}
+fn check_boid_collision(queue: &Queue, boids: QueryMut<(&mut InstanceContainer, &IsDynamic)>) {
+    
+    for (_entity, (game_object, _)) in boids {
         for instance in &mut game_object.instances {
-            instance.position[0] += 0.01;
+            instance.rotation.x += 0.01;
         }
-        game_object.update(&state.queue);
+        game_object.update(queue);
     }
 }
 fn keyboard_input(state: &mut State, event: &KeyboardInput) {
